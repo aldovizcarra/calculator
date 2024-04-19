59578154eb896acf8611ac6615bd201d8c9b5c40
Aldo Vizcarra <avizcarra87@gmail.com>
2024-04-18 18:04:41 -0700

    Add buttons reference

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let num1;
 let num2;
 let operator;
+const btn = document.querySelectorAll(".buttons > *");
 
 function add(num1, num2) {
   return num1 + num2;
@@ -22,4 +23,4 @@ function operate(num1, num2, operator) {
   return operator(num1, num2);
 }
 
-console.log(operate(5, 5, multiply));
+console.log(btn);
